fcaf0e89a7703f0445ebec240a30a95633e50cf6
David Silver <dms.silver@gmail.com>
2014-01-03 18:40:53 -0800

    Basic fox infection completed for prototype work.
    Signed-off-by: David Silver <dms.silver@gmail.com>

--- flexible_survival/data/infections.lua
+++ flexible_survival/data/infections.lua
@@ -22,28 +22,124 @@
 newTalentType{ type="inherent/infections", name = "infections", description = "Nanite infections" }
 
 newTalent{
-	name = "T_HEAD_FOX",
+	name = "T_HEAD_LATEXFOX",
 	type = {"inherent/infections", 1},
 	points = 1,
 	mode = "passive",
 	on_learn = function(self, t)
-		self:learnTalent(self.ENHANCED_SENSES,TRUE,1)
-		self:learnTalent(self.BITE,TRUE,2)
+		self:learnTalent(self.ENHANCED_SENSES,true,1)
+		self:learnTalent(self.BITE,true,2)
 		self.head_desc = "a mixture of canine and human, a perpetual sneer showing off exposed teeth."
 		self.head_species = "fox"
 		self.head_speciesadj = "vulpine"
 		return true
 	end,
 	on_unlearn = function(self, t)
-		self:unlearnTalent(self.ENHANCED_SENSES,TRUE,1)
-		self:unlearnTalent(self.BITE,TRUE,2)
+		self:unlearnTalent(self.ENHANCED_SENSES,true,1)
+		self:unlearnTalent(self.BITE,true,2)
 		self.head_desc = ""
 		self.head_species = ""
 		self.head_speciesadj = ""
 		return true
 	end,
 	info = function(self, t)
-		return "Kick!"
+		return ""
+	end,
+}
+
+newTalent{
+	name = "T_TORSO_LATEXFOX",
+	type = {"inherent/infections", 1},
+	points = 1,
+	mode = "passive",
+	on_learn = function(self, t)
+		self:learnTalent(self.VITALITY,true,1)
+		self.torso_desc = "largely human, with only faint hints of bestial physique."
+		self.torso_species = "human"
+		self.torso_speciesadj = "human"
+		return true
+	end,
+	on_unlearn = function(self, t)
+		self:unlearnTalent(self.VITALITY,true,1)
+		self.torso_desc = ""
+		self.torso_species = ""
+		self.torso_speciesadj = ""
+		return true
+	end,
+	info = function(self, t)
+		return ""
+	end,
+}
+
+newTalent{
+	name = "T_ASS_LATEXFOX",
+	type = {"inherent/infections", 1},
+	points = 1,
+	mode = "passive",
+	on_learn = function(self, t)
+		self.ass_desc = "They have a large fox tail cartoonishly ballooning from their bottom."
+		self.ass_species = "fox"
+		self.ass_speciesadj = "vulpine"
+		return true
+	end,
+	on_unlearn = function(self, t)
+		self.ass_desc = ""
+		self.ass_species = ""
+		self.ass_speciesadj = ""
+		return true
+	end,
+	info = function(self, t)
+		return ""
+	end,
+}
+
+newTalent{
+	name = "T_LEGS_LATEXFOX",
+	type = {"inherent/infections", 1},
+	points = 1,
+	mode = "passive",
+	on_learn = function(self, t)
+		self:learnTalent(self.RUBBER_HIDE,true,1)
+		self.legs_desc = "digitigrade and shaped like dog legs with large paws for feet."
+		self.legs_species = "fox"
+		self.legs_speciesadj = "vulpine"
+		return true
+	end,
+	on_unlearn = function(self, t)
+		self:unlearnTalent(self.RUBBER_HIDE,true,1)
+		self.ass_desc = ""
+		self.ass_species = ""
+		self.ass_speciesadj = ""
+		return true
+	end,
+	info = function(self, t)
+		return ""
+	end,
+}
+
+newTalent{
+	name = "T_SKIN_LATEXFOX",
+	type = {"inherent/infections", 1},
+	points = 1,
+	mode = "passive",
+	on_learn = function(self, t)
+		self:learnTalent(self.RUBBER_HIDE,true,3)
+		self.skin_desc = "red and white latex"
+		self.skin_longdesc= "red and white latex."
+		self.skin_species = "rubber"
+		self.skin_speciesadj = "rubbery"
+		return true
+	end,
+	on_unlearn = function(self, t)
+		self:unlearnTalent(self.RUBBER_HIDE,true,3)
+		self.skin_desc = ""
+		self.skin_longdesc= ""
+		self.skin_species = "human"
+		self.skin_speciesadj = "smooth"
+		return true
+	end,
+	info = function(self, t)
+		return ""
 	end,
 }
 
