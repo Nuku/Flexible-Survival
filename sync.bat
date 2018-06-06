4287a75571dda018f5fd92acdfe3a3577a97dd75
Brim Fox <helicalzero@gmail.com>
2018-06-05 23:39:47 -0700

    adding a batch script to do your sync for you
    ONLY WORKS IF YOU HAVE NAMED YOUR INFORM PROJECT "Flexible Survival"!!

---
+++ sync.bat
@@ -0,0 +1,29 @@
+@echo off
+echo [INFO] Starting...
+echo [INFO] Copying .ctags to user folder
+copy /Y ".ctags" "%HOMEPATH%\.ctags"
+echo [INFO] Copying story.ni to Inform project folder
+copy /Y "Inform\story.ni" "%HOMEPATH%\Documents\Inform\Projects\Flexible Survival.inform\Source\story.ni"
+
+echo [INFO] Copying Inform folder to Inform project folder
+xcopy /I /Q /Y "Inform" "%HOMEPATH%\Documents\Inform\Projects\Flexible Survival.inform\Source"
+
+echo [INFO] Copying Figures folder to Inform project folder
+xcopy /I /Q /Y "Figures" "%HOMEPATH%\Documents\Inform\Projects\Flexible Survival.materials\Figures"
+
+echo [INFO] Copying all folders into the Inform extensions folder that are not Inform or Figures
+for /d %%D in (*) do (
+  IF "%%D"=="Inform" (
+    echo [INFO]   * Skipping Inform folder
+  ) ELSE (
+    IF "%%D"=="Figures" (
+      echo [INFO]   * Skipping Figures folder
+    ) ELSE (
+      echo [INFO]   Copying %%D
+      xcopy /I /Q /Y "%%D" "%HOMEPATH%\Documents\Inform\Extensions\%%D"
+    )
+  )
+)
+
+echo [INFO] Done!
+pause
